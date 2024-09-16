4b687bbf7e2a1a75988fb26b5e2e699dc9aa5a87
Selenao10 <selenao10@nycstudents.net>
2024-09-16 16:31:57 -0400

    Lecture 2

---
+++ Ouyang_Selena_Lecture2.cpp
@@ -0,0 +1,29 @@
+//Selena Ouyang 
+//Lecture 2
+//09-16-2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //Declaring and assigning simple data types
+    int intNum = 11;
+    float floatNum = 11.1;
+    double doubleNum = 3.33;
+    char myLetter = 'S';
+    bool myBoolean = false;
+    string myString = "home";
+
+    cout <<"int: " << intNum << endl;
+    cout <<"float: " << floatNum << endl;
+    cout <<"double: " << doubleNum << endl;
+    cout <<myLetter << endl;
+    cout <<myString << endl;
+//Performing expressions 
+
+
+
+    return 0;
+}
