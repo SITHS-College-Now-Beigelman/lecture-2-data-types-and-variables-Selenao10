7f55c08b955ed598dd72d9644be4c03f03b73b38
Selenao10 <selenao10@nycstudents.net>
2024-09-17 15:56:06 -0400

    Commit

--- Ouyang_Selena_Lecture2.cpp
+++ Ouyang_Selena_Lecture2.cpp
@@ -16,14 +16,22 @@ int main()
     bool myBoolean = false;
     string myString = "home";
 
+    // Outputting simple data types
     cout <<"int: " << intNum << endl;
     cout <<"float: " << floatNum << endl;
     cout <<"double: " << doubleNum << endl;
-    cout <<myLetter << endl;
-    cout <<myString << endl;
-//Performing expressions 
+    cout <<"myLetter: " << myLetter << endl;
+    cout <<"myString: " << myString << endl<< endl;
 
+    //Comparing two characters 
+    char char1 = 'A';
+    char char2 = 'B';
+    cout<< "A < B = " << (char1 >= char2) <<endl <<endl;
 
+    //Perform and outputting expressions:
+    cout << "2 + 3.5 = " << 2 + 3.5 << endl;
+    cout << "6 / 4 +3.9 = " << 6 / 4 +3.9 << endl;
+    cout << "5.4*2 - (13.6 + 18 / 2) " << 5.4*2 - (13.6 + 18 / 2) << endl;
 
     return 0;
 }
